d6ca78372b8db3ededc0e213fd83a33e94cfde4e
haneda-atsushi <hanecci@gmail.com>
2017-10-09 03:35:41 +0900

    Initial commit

---
+++ Assets/Scripts/SimplePostProcessingStackController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.PostProcessing;
+
+public class SimplePostProcessingStackController : MonoBehaviour
+{
+    Camera                         m_Camera;
+    PostProcessingBehaviour        m_PostProcessingBehaviour;
+    public PostProcessingProfile[] m_ProfileArray;
+    int                            m_ProfileIndex = 0;
+
+    void Start ()
+    {
+        m_Camera = Camera.main;
+        Assert.IsNotNull( m_Camera );
+
+        m_PostProcessingBehaviour = m_Camera.GetComponent<PostProcessingBehaviour>();
+        Assert.IsNotNull( m_PostProcessingBehaviour );
+    }
+
+
+    public void SetProfileIndex( int index )
+    {
+        PostProcessingProfile profile     = m_ProfileArray[ index ];
+        m_PostProcessingBehaviour.profile = profile;
+        m_ProfileIndex                    = index;
+    }
+
+
+    public int GetProfileIndex()
+    {
+         return m_ProfileIndex;
+    }
+
+    public int GetProfileNum()
+    {
+        if ( m_ProfileArray != null )
+        {
+            return m_ProfileArray.Length;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public string GetProfileName( int index )
+    {
+        PostProcessingProfile profile = m_ProfileArray[ index ];
+        return profile.name;
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Test : MonoBehaviour
+{
+    SimplePostProcessingStackController m_Controller;
+
+    void Start ()
+    {
+        m_Controller = FindObjectOfType<SimplePostProcessingStackController>();
+        Assert.IsNotNull( m_Controller );
+    }
+
+    void OnGUI()
+    {
+        int x = 200;
+        int y = 200;
+
+        int profile_num = m_Controller.GetProfileNum();
+        for ( int index = 0; index < profile_num; ++ index )
+        {
+            string label = m_Controller.GetProfileName( index );
+            if ( GUI.Button( new Rect( x, y, 200, 100 ), label ) )
+            {
+                m_Controller.SetProfileIndex( index );
+            }
+
+            y += 125;
+        }
+    }
+}
